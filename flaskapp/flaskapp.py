7c829646e4693955045c7614eaf7ade505a56c7a
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2018-01-29 13:37:11 +0000

    fix paths using join

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -2,12 +2,13 @@ from flask import Flask, request, jsonify
 import sys, os, json
 
 app = Flask(__name__)
+db = os.path.join(sys.path[0], "store.txt")
 
 
 #Reads the stored values and outputs them.
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    fh = open("/home/ubuntu/flaskapp/store.txt", "r")
+    fh = open(db, "r")
     return fh.read()
 
 
@@ -23,7 +24,7 @@ def post():
     if( (int(onOff) in [0,1]) and (-180 <= float(turnAngle) <= 180) ):
         
         #Open store.txt in write mode.
-        fh = open("/home/ubuntu/flaskapp/store.txt", "w")
+        fh = open(db, "w")
         
         #Store params as json and write to store.txt.
         jsonDict = json.dumps({"onOff:":int(onOff), "turnAngle":float(turnAngle)})

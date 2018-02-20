47e6989a9fbbbcc454828ec0b3860db591472192
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2018-02-20 12:42:15 +0000

    style(classes): lint code

--- flaskapp/classes.py
+++ flaskapp/classes.py
@@ -1,6 +1,7 @@
-#Describes the item added to the delivery
+# Describes the item added to the delivery
 class Package:
-    def __init__(self, id, name, description, priority, minTemp, maxTemp, timeLimit):
+    def __init__(self, id, name, description, priority, minTemp,
+                 maxTemp, timeLimit):
         self.id = id
         self.name = name
         self.description = description
@@ -13,14 +14,16 @@ class Package:
         if(timeLimit < 0):
             raise ValueError("Invalid time limit")
 
-#A delivery contains packages
+
+# A delivery contains packages
 class Delivery:
     def __init__(self, id, packageList, fromLoc, toLoc, state):
         self.id = id
         self.packageList = packageList
         self.fromLoc = fromLoc
         self.toLoc = toLoc
-        self.priority = min([x.priority for x in packageList]) #lowest number highest priority
+        # Lowest number has highest priority
+        self.priority = min([x.priority for x in packageList])
         self.state = state
         if(len(packageList) < 1):
             raise ValueError("No packages")
